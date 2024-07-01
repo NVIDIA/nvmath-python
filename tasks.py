20a8179fa164cfaeeb62faae407c681f970c0179
Leo Fang <leof@nvidia.com>
2024-07-01 17:00:38 -0400

    Add missing header preamble

--- tasks.py
+++ tasks.py
@@ -1,5 +1,8 @@
-from invoke import Collection, task
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: Apache-2.0
 
+from invoke import Collection, task
 
 
 @task
